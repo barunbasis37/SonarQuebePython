25f0574406ff78dd63aa75f63c31ba75837b3975
Barun Kanti Das <barunbasis37@gmail.com>
2025-11-14 23:06:52 +0600

    Modify And Add Screts4

--- python.py
+++ python.py
@@ -1,16 +1,2 @@
 from flask import Flask, render_template_string
-
-# python.py
-
-app = Flask(__name__)
-
-@app.route('/')
-def index():
-    return render_template_string("""<!doctype html>
-<html>
-<body><h1>Hello {{ name }}</h1>
-<p>{{ message }}</p></body>
-</html>""", name="World", message="This is Python")
-
-if __name__ == '__main__':
-    app.run(debug=True)
+print("Hello, World!")
