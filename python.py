b5d86b6995b6c506c62443f939b054bbe7140728
Barun Kanti Das <barunbasis37@gmail.com>
2025-11-14 23:02:41 +0600

    Modify And Add Screts3

--- python.py
+++ python.py
@@ -1,2 +1,16 @@
-print("Hellow World! In SonarQuebe")
-print("Develo By Barun")
+from flask import Flask, render_template_string
+
+# python.py
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template_string("""<!doctype html>
+<html>
+<body><h1>Hello {{ name }}</h1>
+<p>{{ message }}</p></body>
+</html>""", name="World", message="This is Python")
+
+if __name__ == '__main__':
+    app.run(debug=True)
